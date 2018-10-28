8643eee04bce97d624cbfd4f49829726f5bdbd32
Morganna Giovanelli <morgcontato@gmail.com>
2018-10-28 15:51:35 -0300

    Working with operators

---
+++ operadores matemáticos/operadores.js
@@ -0,0 +1,11 @@
+// operadores binários: operando + operador + operando
+10 + 10;
+
+// operadores unários: operando + operador ou operador + operando
+10++;
+++10;
+
+// ++ antes, soma antes
+// ++ depois, soma depois
+
+// operadores: + - * /
