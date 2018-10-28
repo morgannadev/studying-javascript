210a456b2771d18764653a05f4f5eb782d46deca
Morganna Giovanelli <morgcontato@gmail.com>
2018-10-28 14:35:53 -0300

    Organizing items and understading types

---
+++ tipos de variáveis/tipos.js
@@ -0,0 +1,12 @@
+var teste = 1 // number
+teste = 10.2 // number
+teste = true // boolean
+teste = "teste" // string
+teste = "1" // string
+teste = "true" // string
+
+var valor1 = "1"
+valor + 1; // será igual a "11"
+
+var valor2 = 1
+valor + 1; // será igual a 2
